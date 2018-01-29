bf7ad26eecc2478dbfb9d9f256ffa1b20cfe1ea3
WU ZHILONG <13412906+wuzhilon@users.noreply.github.com>
2018-01-29 16:28:47 +0800

    Update SparkDataCollection.scala

--- app/org/apache/spark/deploy/history/SparkDataCollection.scala
+++ app/org/apache/spark/deploy/history/SparkDataCollection.scala
@@ -50,7 +50,7 @@ class SparkDataCollection extends SparkApplicationData {
   lazy val jobProgressListener = new JobProgressListener(new SparkConf())
   lazy val environmentListener = new EnvironmentListener()
   lazy val storageStatusListener = new StorageStatusListener(new SparkConf())
-  lazy val executorsListener = new ExecutorsListener(storageStatusListener, new SparkConf())
+  lazy val executorsListener = new ExecutorsListener(storageStatusListener,new SparkConf())
   lazy val storageListener = new StorageListener(storageStatusListener)
 
   // This is a customized listener that tracks peak used memory
@@ -164,13 +164,14 @@ class SparkDataCollection extends SparkApplicationData {
     if (_executorData == null) {
       _executorData = new SparkExecutorData()
 
+      //for (statusId <- 0 until executorsListener.storageStatusList.size) {
       val  storageStatusList = executorsListener.activeStorageStatusList ++ executorsListener.deadStorageStatusList
-
-      for (statusId <- 0 until storageStatusList.size) {
+      for (statusId <- 0 until storageStatusList.size) {  
         val info = new ExecutorInfo()
 
+        //val status = executorsListener.storageStatusList(statusId)
         val status = storageStatusList(statusId)
-
+        
         info.execId = status.blockManagerId.executorId
         info.hostPort = status.blockManagerId.hostPort
         info.rddBlocks = status.numBlocks
@@ -180,16 +181,22 @@ class SparkDataCollection extends SparkApplicationData {
         info.memUsed = storageStatusTrackingListener.executorIdToMaxUsedMem.getOrElse(info.execId, 0L)
         info.maxMem = status.maxMem
         info.diskUsed = status.diskUsed
-        info.activeTasks = executorsListener.executorToTasksActive.getOrElse(info.execId, 0)
-        info.failedTasks = executorsListener.executorToTasksFailed.getOrElse(info.execId, 0)
-        info.completedTasks = executorsListener.executorToTasksComplete.getOrElse(info.execId, 0)
-        info.totalTasks = info.activeTasks + info.failedTasks + info.completedTasks
-        info.duration = executorsListener.executorToDuration.getOrElse(info.execId, 0L)
-        info.inputBytes = executorsListener.executorToInputBytes.getOrElse(info.execId, 0L)
-        info.shuffleRead = executorsListener.executorToShuffleRead.getOrElse(info.execId, 0L)
-        info.shuffleWrite = executorsListener.executorToShuffleWrite.getOrElse(info.execId, 0L)
-
-        _executorData.setExecutorInfo(info.execId, info)
+        
+        val executorToTaskSummary = executorsListener.executorToTaskSummary
+        //info.failedTasks = executorsListener.executorEvents
+        val executorTaskSummary = executorToTaskSummary.getOrElse(info.execId, null)
+        if(executorTaskSummary != null ){
+          info.activeTasks = executorTaskSummary.tasksActive
+          info.failedTasks = executorTaskSummary.tasksFailed
+          info.completedTasks = executorTaskSummary.tasksComplete
+          info.totalTasks =  info.activeTasks + info.failedTasks + info.completedTasks
+          info.duration = executorTaskSummary.duration
+          info.inputBytes = executorTaskSummary.inputBytes
+          info.shuffleRead = executorTaskSummary.shuffleRead
+          info.shuffleWrite = executorTaskSummary.shuffleWrite
+          _executorData.setExecutorInfo(info.execId, info)
+        }
+        
       }
     }
     _executorData
