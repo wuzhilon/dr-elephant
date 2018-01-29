792138e5429bc7f594497f7b90e3afcd8ecc5306
WU ZHILONG <13412906+wuzhilon@users.noreply.github.com>
2018-01-29 16:27:26 +0800

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -29,8 +29,8 @@ function play_command() {
 }
 
 # Default configurations
-HADOOP_VERSION="2.3.0"
-SPARK_VERSION="1.4.0"
+HADOOP_VERSION="2.6.0"
+SPARK_VERSION="2.2.0"
 
 # User should pass an optional argument which is a path to config file
 if [ -z "$1" ];
