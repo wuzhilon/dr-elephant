507d74070269281eacdb73e58b970696d5b25118
WU ZHILONG <13412906+wuzhilon@users.noreply.github.com>
2018-01-29 16:28:22 +0800

    Update StorageStatusTrackingListener.scala

--- app/org/apache/spark/storage/StorageStatusTrackingListener.scala
+++ app/org/apache/spark/storage/StorageStatusTrackingListener.scala
@@ -77,6 +77,7 @@ class StorageStatusTrackingListener extends SparkListener {
       val info = taskEnd.taskInfo
       val metrics = taskEnd.taskMetrics
       if (info != null && metrics != null) {
+        //val updatedBlocks = metrics.updatedBlocks.getOrElse(Seq[(BlockId, BlockStatus)]())
         val updatedBlocks = metrics.updatedBlockStatuses
         if (updatedBlocks.length > 0) {
           updateStorageStatus(info.executorId, updatedBlocks)
@@ -96,7 +97,7 @@ class StorageStatusTrackingListener extends SparkListener {
       val blockManagerId = blockManagerAdded.blockManagerId
       val executorId = blockManagerId.executorId
       val maxMem = blockManagerAdded.maxMem
-      val storageStatus = new StorageStatus(blockManagerId, maxMem)
+      val storageStatus = new StorageStatus(blockManagerId, maxMem,blockManagerAdded.maxOnHeapMem,blockManagerAdded.maxOffHeapMem)
       executorIdToStorageStatus(executorId) = storageStatus
     }
   }
